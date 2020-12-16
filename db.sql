5316eaee28cdfe2d0d214d262c3b3f415c49d8bf
Luke Roy <luke@Lukes-MBP.fritz.box>
2020-12-16 15:16:41 +0100

    add updated database sql demo.env for testing, updated compose file

--- db.sql
+++ db.sql
@@ -2,16 +2,10 @@ CREATE TABLE users (
                        id serial PRIMARY KEY ,
                        password VARCHAR ( 128 ) NOT NULL,
                        email VARCHAR ( 255 ) UNIQUE NOT NULL,
+                       first_name varchar (255) NOT NUll,
+                       last_name varchar (255) NOT NUll,
                        type VARCHAR ( 255 ) NOT NULL DEFAULT 'user',
+                       confirmed BOOLEAN NOT NULL DEFAULT false ,
                        created_at TIMESTAMP NOT NULL,
                        updated_at TIMESTAMP NOT NULL
-);
-
-CREATE TABLE accounts (
-                          user_id serial PRIMARY KEY,
-                          password VARCHAR ( 128 ) NOT NULL,
-                          email VARCHAR ( 255 ) UNIQUE NOT NULL,
-                          type VARCHAR ( 255 ) NOT NULL DEFAULT ,
-                          created_on TIMESTAMP NOT NULL,
-                          last_login TIMESTAMP
 );
